fecc56fa3f27a2a02815a613a5b7c17cafc55cbf
Afonso Gesualdi <152401918+gesualdiafonso@users.noreply.github.com>
2025-06-27 19:50:33 -0300

    Update Connection.php

--- includes/utilities/models/Connection.php
+++ includes/utilities/models/Connection.php
@@ -3,7 +3,7 @@ class Connection
 {
     private const DB_HOST = 'localhost';
     private const DB_USER = 'root';
-    private const DB_PASS = 'root';
+    private const DB_PASS = '';
     private const DB_NAME = 'malba_db';
 
     private const DB_DSN = 'mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME . ';charset=utf8mb4';
